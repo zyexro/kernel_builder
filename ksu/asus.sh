490a71286e98b2f384937b13941ac8a83ffb79fe
zyexro <zyexro@proton.me>
2026-06-05 11:40:55 +0100

    fixup! update susfs v2.1.0

--- ksu/asus.sh
+++ ksu/asus.sh
@@ -8,7 +8,7 @@ source "${outside}/$1env"
 
 curl -LSs "https://raw.githubusercontent.com/zyexro/KernelSU/master/kernel/setup.sh" | bash -s master
 git add . && git commit -am "drivers: KernelSU"
-KSU_git_ver=$(cd KernelSU-Next && git rev-list --count HEAD)
+KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 30000))
 
 patchesdir="$outside/ksu/patches/"
