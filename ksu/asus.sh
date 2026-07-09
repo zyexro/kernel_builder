3e1fe6451388cccefc18f2e7bbc75747176c8f1b
zyexro <zyexro@proton.me>
2026-07-09 14:03:30 +0100

    update susfs
    Signed-off-by: zyexro <zyexro@proton.me>

--- ksu/asus.sh
+++ ksu/asus.sh
@@ -38,5 +38,5 @@ sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}sus\"/"
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
 
 echo -e " \nincludes backslahxx's KernelSU fork, ver ${KSU_ver}" >> banner_append
-echo -e " \nincludes SuSFS v2.1.0" >> banner_append
+echo -e " \nincludes SuSFS v2.2" >> banner_append
 
