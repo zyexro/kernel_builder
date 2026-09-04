f097d28dc99fe7534b5ba554e140a4fc3bcfe75e
zyexro <zyexro@proton.me>
2026-09-04 21:32:00 +0100

    ksu: switch to ResukiSU
    Signed-off-by: zyexro <zyexro@proton.me>

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -6,13 +6,14 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
-curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -s master
+curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash -s main
 git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 30000))
 
-echo 'CONFIG_KSU_EXTRAS=y' >> "${defconfig_file}"
 patchesdir="$outside/ksu/patches/"
+
+echo 'CONFIG_KSU_MANUAL_HOOK=y' >> "${defconfig_file}"
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
     git am "$patch_file"
--- ksu/asus.sh
+++ ksu/asus.sh
@@ -6,7 +6,7 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
-curl -LSs "https://raw.githubusercontent.com/zyexro/KernelSU/master/kernel/setup.sh" | bash -s master
+curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash -s main
 git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 30000))
@@ -14,7 +14,8 @@ KSU_ver=$(($KSU_git_ver + 30000))
 patchesdir="$outside/ksu/patches/"
 suspatchesdir="$outside/ksu/sus_patches/"
 
-echo 'CONFIG_KSU_EXTRAS=y' >> "${defconfig_file}"
+echo 'CONFIG_KSU_MANUAL_HOOK=y' >> "${defconfig_file}"
+echo 'CONFIG_KSU_SUSFS=y' >> "${defconfig_file}"
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
     git am "$patch_file"
